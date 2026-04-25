34a0582094baa56e59fdc550ab9222ef1395bef7
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-04-25 09:25:59 -0700

    Updates to the Launcher

--- widget/Launcher.tsx
+++ widget/Launcher.tsx
@@ -1,13 +1,30 @@
 import Gtk from "gi://Gtk?version=4.0"
-import GLib from "gi://GLib"
+import Apps from "gi://AstalApps"
 import PopupWindow from "./PopupWindow"
-import { createPoll } from "ags/time"
 import { Astal } from "ags/gtk4"
 
 
+// Define what a single app row looks like
+function AppItem({ app }: { app: Apps.Application }) {
+    return (
+        <button
+            cssName="app-item"
+            onClicked={() => {
+                app.launch() // Opens the app
+            }}>
+            <box>
+                <box cssName="app-icon-wrapper">
+                    <image iconName={app.iconName || "image-missing"} cssName="app-icon" />
+                </box>
+                <label label={app.name} cssName="app-name" />
+            </box>
+        </button>
+    )
+}
 
 export function Launcher() {
 
+    const apps = new Apps.Apps();
 
     const popup = new PopupWindow({
         name: "launcher-detail-window",
@@ -16,7 +33,19 @@ export function Launcher() {
         margin: 8,
         child: (
             <box cssName="launcher-detail-container" orientation={Gtk.Orientation.VERTICAL}>
-                
+                <entry
+                    placeholderText="Search..."
+                // onChanged={({ text }) => {
+                //     // Logic to filter app list based on input
+                // }}
+                />
+                <scrolledwindow vexpand heightRequest={400}>
+                    <box orientation={Gtk.Orientation.VERTICAL}>
+                        {apps.get_list().map((app: Apps.Application) => (
+                            <AppItem app={app} />
+                        ))}
+                    </box>
+                </scrolledwindow>
             </box>
         )
     });
