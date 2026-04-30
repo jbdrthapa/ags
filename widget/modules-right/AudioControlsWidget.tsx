f9f84b7cd540b6a8fab01a2cf049dc410ab956ca
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-04-29 19:28:55 -0700

    Adding audiocontrols sliders for speaker and microphone

--- widget/modules-right/AudioControlsWidget.tsx
+++ widget/modules-right/AudioControlsWidget.tsx
@@ -1,9 +1,44 @@
+import AstalWp from "gi://AstalWp?version=0.1";
 import Gtk from "gi://Gtk?version=4.0";
+import { createBinding } from "ags"
 
 export function AudioControlsWidget() {
+
+    const { defaultSpeaker: speaker } = AstalWp.get_default()!
+    const { defaultMicrophone: microphone } = AstalWp.get_default()!
+
+
+    // simple component for a slider with an icon, used for both speaker and microphone
+    function SliderSet({ endpoint }: { endpoint: AstalWp.Endpoint }) {
+
+        const endpointName = endpoint.name || "Unknown"
+        const volumeIcon = createBinding(endpoint, "volumeIcon");
+        const volume = createBinding(endpoint, "volume");
+        const volumeText = volume.as((value) => String(Math.trunc(value * 100)) + "%");
+
+        return (
+            <box orientation={Gtk.Orientation.HORIZONTAL}>
+                <image
+                    iconSize={Gtk.IconSize.LARGE}
+                    iconName={volumeIcon} cssName={"audio-icon"} />
+                <label label={volumeText} widthRequest={80} cssName={"audio-percent"} />
+                <slider
+                    tooltipText={volumeText}
+                    widthRequest={360}
+                    heightRequest={40}
+                    onChangeValue={({ value }) => endpoint.set_volume(value)}
+                    value={createBinding(endpoint, "volume")} />
+            </box>
+        );
+    }
+
     return (
-        <box orientation={Gtk.Orientation.HORIZONTAL} cssName="audio-controls-container">
-            
+
+
+
+        <box orientation={Gtk.Orientation.VERTICAL} cssName="audio-controls-container">
+            <SliderSet endpoint={speaker} />
+            <SliderSet endpoint={microphone} />
         </box>
     );
 }
