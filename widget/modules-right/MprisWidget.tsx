ffab1cc3c91b84b33d55a71a12bc8cf267105159
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-04-29 21:17:31 -0700

    Refactoring, styling MprisWidget

--- widget/modules-right/MprisWidget.tsx
+++ widget/modules-right/MprisWidget.tsx
@@ -1,70 +1,105 @@
 import AstalMpris from "gi://AstalMpris?version=0.1";
-import AstalApps from "gi://AstalApps?version=0.1"
 import Gtk from "gi://Gtk?version=4.0";
 import { For, createBinding } from "ags"
 
 export function MprisWidget() {
 
     const mpris = AstalMpris.get_default()
-    const apps = new AstalApps.Apps()
     const players = createBinding(mpris, "players")
+
+    // function returns media cover art, if available, otherwise returns a default icon
+
+    function MediaCoverArt({ mprisPlayer }: { mprisPlayer: AstalMpris.Player }) {
+
+        const coverArt = createBinding(mprisPlayer, "coverArt")
+
+        return (
+            <box overflow={Gtk.Overflow.HIDDEN} cssName="mpris-cover-art">
+                <image pixelSize={64} file={coverArt} />
+            </box>
+        );
+    }
+
+    // Function returns media information such as title and artist, and truncates it if it's too long
+
+    function MediaInfo({ mprisPlayer }: { mprisPlayer: AstalMpris.Player }) {
+
+        const textWidth = 30;
+
+        const title = createBinding(mprisPlayer, "title")
+
+        const titleText = title.as((value) => {
+            const text = String(value ?? "")
+            return text.length > textWidth ? text.slice(0, textWidth) + "…" : text
+        })
+
+        const artist = createBinding(mprisPlayer, "artist")
+
+        const artistText = artist.as((value) => {
+            const text = String(value ?? "")
+            return text.length > textWidth ? text.slice(0, textWidth) + "…" : text
+        })
+
+        return (
+            <box valign={Gtk.Align.CENTER} halign={Gtk.Align.FILL} orientation={Gtk.Orientation.VERTICAL} cssName={"mpris-media-info"}>
+                <label xalign={0} label={titleText} cssName={"mpris-title"} />
+                <label xalign={0} label={artist} cssName={"mpris-artist"} />
+            </box>
+        );
+    }
+
+    // Function returns media playback buttons such as previous, play/pause, and next, and only shows them if the player supports them
+
+    function MediaPlaybackButtons({ mprisPlayer }: { mprisPlayer: AstalMpris.Player }) {
+
+        const canGoPrevious = createBinding(mprisPlayer, "canGoPrevious")
+        const canControl = createBinding(mprisPlayer, "canControl")
+        const canGoNext = createBinding(mprisPlayer, "canGoNext")
+        const playbackStatus = createBinding(mprisPlayer, "playbackStatus",)((s) => s === AstalMpris.PlaybackStatus.PLAYING)
+        const playbackStatusNot = playbackStatus.as((status) => !status)
+
+
+        return (
+            <box hexpand spacing={2} halign={Gtk.Align.END} valign={Gtk.Align.CENTER}>
+
+                <button onClicked={() => mprisPlayer.previous()} visible={canGoPrevious} cssName="mpris-button">
+                    <image iconName="media-seek-backward-symbolic" />
+                </button>
+
+                <button onClicked={() => mprisPlayer.play_pause()} visible={canControl} cssName="mpris-button">
+                    <box>
+                        <image iconName="media-playback-start-symbolic" visible={playbackStatus} />
+                        <image iconName="media-playback-pause-symbolic" visible={playbackStatusNot} />
+                    </box>
+                </button>
+
+                <button onClicked={() => mprisPlayer.next()} visible={canGoNext} cssName="mpris-button">
+                    <image iconName="media-seek-forward-symbolic" />
+                </button>
+
+            </box>
+        );
+    }
+
 
     return (
 
-        <menubutton>
-            <box>
-                <For each={players}>
-                    {(player) => {
-                        const [app] = apps.exact_query(player.entry)
-                        return <image visible={!!app.iconName} iconName={app?.iconName} />
-                    }}
-                </For>
-            </box>
-            <popover>
-                <box spacing={4} orientation={Gtk.Orientation.VERTICAL}>
-                    <For each={players}>
-                        {(player) => (
-                            <box spacing={4} widthRequest={200}>
+        <box spacing={20} orientation={Gtk.Orientation.VERTICAL}>
+            <For each={players}>
+                {(player) => (
 
-                                {/* Cover art */}
+                    <box>
+                        {/* Cover art */}
+                        {MediaCoverArt({ mprisPlayer: player })}
 
-                                <box overflow={Gtk.Overflow.HIDDEN} css="border-radius: 8px;">
-                                    <image pixelSize={64} file={createBinding(player, "coverArt")} />
-                                </box>
+                        {/* Title and Artist */}
+                        {MediaInfo({ mprisPlayer: player })}
 
-                                {/* Title and Artist */}
-
-                                <box valign={Gtk.Align.CENTER} orientation={Gtk.Orientation.VERTICAL}>
-                                    <label xalign={0} label={createBinding(player, "title")} />
-                                    <label xalign={0} label={createBinding(player, "artist")} />
-                                </box>
-
-                                {/* Previous, Play/Pause, Next buttons */}
-
-                                <box hexpand halign={Gtk.Align.END}>
-
-                                    <button onClicked={() => player.previous()} visible={createBinding(player, "canGoPrevious")}>
-                                        <image iconName="media-seek-backward-symbolic" />
-                                    </button>
-
-                                    <button onClicked={() => player.play_pause()} visible={createBinding(player, "canControl")}>
-                                        <box>
-                                            <image iconName="media-playback-start-symbolic" visible={createBinding(player, "playbackStatus",)((s) => s === AstalMpris.PlaybackStatus.PLAYING)} />
-                                            <image iconName="media-playback-pause-symbolic" visible={createBinding(player, "playbackStatus",)((s) => s !== AstalMpris.PlaybackStatus.PLAYING)} />
-                                        </box>
-                                    </button>
-
-                                    <button onClicked={() => player.next()} visible={createBinding(player, "canGoNext")}>
-                                        <image iconName="media-seek-forward-symbolic" />
-                                    </button>
-
-                                </box>
-
-                            </box>
-                        )}
-                    </For>
-                </box>
-            </popover>
-        </menubutton>
+                        {/* Previous, Play/Pause, Next buttons */}
+                        {MediaPlaybackButtons({ mprisPlayer: player })}
+                    </box>
+                )}
+            </For>
+        </box>
     );
 }
