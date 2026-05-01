2f1478bcdd30318063e754ad193f0e74097c18d9
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-05-01 11:51:21 -0700

    Pill popover summary styling and addition of some wifi properties.

--- widget/modules-right/WirelessPillWidget.tsx
+++ widget/modules-right/WirelessPillWidget.tsx
@@ -2,10 +2,11 @@ import Gtk from "gi://Gtk?version=4.0";
 import GLib from 'gi://GLib';
 import AstalNetwork from "gi://AstalNetwork"
 import { For, With, createBinding } from "ags"
-import { execAsync } from "ags/process"
+import { exec, execAsync } from "ags/process"
+import { createPoll } from "ags/time";
 
 export function WirelessPillWidget() {
-
+    const wifi_network_device_name = "wlp99s0"
     const network = AstalNetwork.get_default()
     const wifi = createBinding(network, "wifi")
 
@@ -24,6 +25,29 @@ export function WirelessPillWidget() {
         }
     }
 
+    function networkProperties(caption: string, value: string) {
+        return (
+            <box orientation={Gtk.Orientation.HORIZONTAL}>
+                <label label={caption} cssName="pill-summary-property-name" />
+                <label label={value} cssName="pill-summary-property-value" />
+            </box>
+        )
+    }
+
+    function networkPropertiesBound<K extends keyof AstalNetwork.Wifi>(caption: string, object: AstalNetwork.Wifi, property: K) {
+        // createBinding returns a Binding object for that specific property
+        const binding = createBinding(object, property as any)
+
+        return (
+            <box orientation={Gtk.Orientation.HORIZONTAL}>
+                <label label={caption} cssName="pill-summary-property-name" />
+
+                {/* Use .as() to ensure numeric values are cast to strings for the label */}
+                <label label={binding.as(val => String(val))} cssName="pill-summary-property-value" />
+            </box>
+        )
+    }
+
     return (
         <box visible={wifi(Boolean)}>
             <With value={wifi}>
@@ -32,18 +56,30 @@ export function WirelessPillWidget() {
                         <menubutton cssName="pill-button">
                             <image iconName={createBinding(wifi, "iconName")} cssName={"pill-button-image"} />
                             <popover>
-                                <box orientation={Gtk.Orientation.VERTICAL}>
-                                    <For each={createBinding(wifi, "accessPoints")(sorted)}>
-                                        {(ap: AstalNetwork.AccessPoint) => (
-                                            <button onClicked={() => connect(ap)}>
-                                                <box spacing={4}>
-                                                    <image iconName={createBinding(ap, "iconName")} />
-                                                    <label label={createBinding(ap, "ssid")} />
-                                                    <image iconName="object-select-symbolic" visible={createBinding(wifi, "activeAccessPoint",)((active) => active === ap)} />
-                                                </box>
-                                            </button>
-                                        )}
-                                    </For>
+                                <box orientation={Gtk.Orientation.VERTICAL} heightRequest={400} cssName="pill-popover-container">
+
+                                    {/* Current WiFi connection details */}
+
+                                    <box orientation={Gtk.Orientation.VERTICAL}>
+                                        {networkPropertiesBound("Network", wifi, "ssid")}
+                                        {networkPropertiesBound("Strength", wifi, "strength")}
+                                    </box>
+
+                                    {/* Access point browse list */}
+                                    <box orientation={Gtk.Orientation.VERTICAL} cssName="pill-popover-details-area-container">
+                                        <label label="Available Access Points" cssName={"pill-popover-details-header"} />
+                                        <For each={createBinding(wifi, "accessPoints")(sorted)}>
+                                            {(ap: AstalNetwork.AccessPoint) => (
+                                                <button onClicked={() => connect(ap)} cssName="wifi-access-point-item">
+                                                    <box spacing={4}>
+                                                        <image iconName={createBinding(ap, "iconName")} />
+                                                        <label label={createBinding(ap, "ssid")} />
+                                                        <image iconName="object-select-symbolic" visible={createBinding(wifi, "activeAccessPoint",)((active) => active === ap)} />
+                                                    </box>
+                                                </button>
+                                            )}
+                                        </For>
+                                    </box>
                                 </box>
                             </popover>
                         </menubutton>
