b982654ffb6855c48b39e5422b61dd90bda9918e
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-04-30 17:59:32 -0700

    Adding commands for system buttons.

--- widget/modules-right/SystemInfoWidget.tsx
+++ widget/modules-right/SystemInfoWidget.tsx
@@ -7,27 +7,31 @@ export function SystemInfoWidget() {
 
     return (
         <box orientation={Gtk.Orientation.HORIZONTAL} cssName="system-info-container">
-            
+
             <box hexpand={true} halign={Gtk.Align.START}>
                 <image iconSize={Gtk.IconSize.LARGE}></image>
             </box>
-            
+
             <box halign={Gtk.Align.END}>
-                
+
                 <button label="" cssName="system-info-button" onClicked={() => {
-                    GLib.spawn_command_line_async('notify-send "Restarting menu"');
+                    GLib.spawn_command_line_async('bash -c "ags quit; ags run"');
                 }} />
 
                 <button label="" cssName="system-info-button" onClicked={() => {
-                    GLib.spawn_command_line_async('notify-send "Locking Screen"');
+                    GLib.spawn_command_line_async('bash -c "swaylock"');
+                }} />
+
+                <button label="󰿅" cssName="system-info-button" onClicked={() => {
+                    GLib.spawn_command_line_async('bash -c "niri msg action quit --skip-confirmation"');
                 }} />
 
                 <button label="" cssName="system-info-button" onClicked={() => {
-                    GLib.spawn_command_line_async('notify-send "Restarting System"');
+                    GLib.spawn_command_line_async('bash -c "systemctl reboot"');
                 }} />
 
                 <button label="" cssName="system-info-button" onClicked={() => {
-                    GLib.spawn_command_line_async('notify-send "Powering Off"');
+                    GLib.spawn_command_line_async('bash -c "systemctl poweroff"');
                 }} />
 
             </box>
