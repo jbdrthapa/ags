b8e0aa5705ea2db751123c832cb65ac350097c16
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-05-02 17:14:57 -0700

    Adding support for Niri workspaces, active workspace, active window icon and title.

--- widget/bar/WorkspaceWidget.tsx
+++ widget/bar/WorkspaceWidget.tsx
@@ -1,10 +1,11 @@
+import Gtk from "gi://Gtk?version=4.0"
 import AstalNiri from "gi://AstalNiri"
 import { For, createBinding } from "ags"
 
 interface NiriWorkspace {
-    id: number
-    name: string | null
-    focus(): void
+  id: number
+  name: string | null
+  focus(): void
 }
 
 export function WorkspaceWidget() {
@@ -13,25 +14,46 @@ export function WorkspaceWidget() {
   // 1. Create a binding for the list of all workspaces
   const workspaces = createBinding(niri, "workspaces")
 
-  // 2. Create a binding for the currently focused workspace object
-  const focused = createBinding(niri, "focused_workspace")
+  // 2. Create a binding for the currently focused window object
+  const focused = createBinding(niri, "focused_window")
 
   return (
-    <box cssName="workspace-container">
-      {/* 3. Use <For> to iterate through the workspaces binding */}
-      <For each={workspaces}>
-        {(ws: NiriWorkspace) => (
-          <button
-            // 4. Transform the focused binding to check if IDs match
-            cssName={focused((f) =>
-              f?.id === ws.id ? "workspace-item active" : "workspace-item"
+    <box>
+      <box cssName="workspace-container" orientation={Gtk.Orientation.HORIZONTAL}>
+        <box spacing={5}>
+          <For each={workspaces}>
+            {(ws: NiriWorkspace) => (
+              <button valign={Gtk.Align.CENTER}
+                cssClasses={createBinding(niri, "focused_workspace").as(focused => {
+                  const isFocused = focused?.id === ws.id;
+                  return isFocused ? ["workspace-item", "focused"] : ["workspace-item"];
+                })}
+                onClicked={() => ws.focus()}
+              >
+                <label label={ws.name || `${ws.id}`} />
+              </button>
             )}
-            onClicked={() => ws.focus()}
-          >
-            <label label={ws.name || `${ws.id}`} />
-          </button>
-        )}
-      </For>
+          </For>
+        </box>
+      </box>
+
+      <box spacing={8} cssName="active-window-container" marginStart={5}>
+
+        {/* Application Icon */}
+        <image visible={focused.as(win => !!win)} icon-name={focused.as(win => win?.app_id || "image-missing")} cssName="active-window-icon" />
+
+        {/* Application Title */}
+        <label cssName={"active-window-name"}
+          label={focused.as(win => {
+            if (!win || !win.title) return "";
+
+            const maxLength = 60;
+            return win.title.length > maxLength
+              ? win.title.substring(0, maxLength) + "..."
+              : win.title;
+          })}
+        />
+      </box>
     </box>
   )
 }
