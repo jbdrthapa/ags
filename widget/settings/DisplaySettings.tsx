759fc1483c93b37646c82187acc2f78f34456344
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-05-11 15:47:14 -0700

    Adding styles for sections and section headings

--- widget/settings/DisplaySettings.tsx
+++ widget/settings/DisplaySettings.tsx
@@ -7,50 +7,47 @@ export function DisplaySettings() {
     const display = Gdk.Display.get_default();
     const monitors = () => display?.get_monitors() ?? [];
 
-    // console.log(`Initial monitors: ${monitors.get_n_items()}`);
-
-
     return (
         <box orientation={Gtk.Orientation.VERTICAL} spacing={10}>
 
-            <label label="Displays" halign={Gtk.Align.START} />
+            <label label="Display Properties" halign={Gtk.Align.START} cssName="section-heading" />
 
             <For each={monitors}>
                 {(monitor) => (
-                    <box spacing={6} orientation={Gtk.Orientation.VERTICAL}>
+                    <box spacing={10} orientation={Gtk.Orientation.VERTICAL} cssName="section-background">
 
                         <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                            <label label="Connector" cssName="settings-param-caption" />
+                            <label label="Connector" xalign={0} cssName="settings-param-caption" />
                             <label label={createBinding(monitor, "connector")} cssName="settings-param-value" halign={Gtk.Align.START} />
                         </box>
 
                         <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                            <label label="Description" cssName="settings-param-caption"/>
+                            <label label="Description" xalign={0} cssName="settings-param-caption" />
                             <label label={createBinding(monitor, "description")} cssName="settings-param-value" halign={Gtk.Align.START} />
                         </box>
 
                         <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                            <label label="Manufacturer" cssName="settings-param-caption"/>
+                            <label label="Manufacturer" xalign={0} cssName="settings-param-caption" />
                             <label label={createBinding(monitor, "manufacturer")} cssName="settings-param-value" halign={Gtk.Align.START} />
                         </box>
 
                         <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                            <label label="Model" cssName="settings-param-caption"/>
+                            <label label="Model" xalign={0} cssName="settings-param-caption" />
                             <label label={createBinding(monitor, "model")} cssName="settings-param-value" halign={Gtk.Align.START} />
                         </box>
 
                         <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                            <label label="Scale" cssName="settings-param-caption"/>
+                            <label label="Scale" xalign={0} cssName="settings-param-caption" />
                             <label label={createBinding(monitor, "scale").as(val => String(val))} cssName="settings-param-value" halign={Gtk.Align.START} />
                         </box>
 
                         <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                            <label label="Refresh Rate" cssName="settings-param-caption"/>
+                            <label label="Refresh Rate" xalign={0} cssName="settings-param-caption" />
                             <label label={createBinding(monitor, "refresh-rate").as(val => String(val))} cssName="settings-param-value" halign={Gtk.Align.START} />
                         </box>
 
                         <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                            <label label="geometry" cssName="settings-param-caption"/>
+                            <label label="geometry" xalign={0} cssName="settings-param-caption" />
                             <label
                                 label={createBinding(monitor, "geometry").as(geo => {
                                     const scale = monitor.get_scale();
