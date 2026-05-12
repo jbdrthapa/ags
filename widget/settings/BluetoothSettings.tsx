161ee84e13e8ca327b45b86a3b287f71dc45feea
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-05-11 18:32:39 -0700

    Adding some bluetooth actions to toggle power, pair, discover modes.

--- widget/settings/BluetoothSettings.tsx
+++ widget/settings/BluetoothSettings.tsx
@@ -2,13 +2,14 @@ import Gtk from "gi://Gtk?version=4.0";
 import AstalBluetooth from "gi://AstalBluetooth"
 import { createBinding } from "gnim";
 
-function BindBluetoothAdapterProperty<prop_key extends keyof AstalBluetooth.Adapter>(caption: string, object: AstalBluetooth.Adapter, property: prop_key) {
+function BindBluetoothAdapterProperty<prop_key extends keyof AstalBluetooth.Adapter>(caption: string, object: AstalBluetooth.Adapter, property: prop_key, buttonVisible: boolean, buttonCaption: string, onButtonClick: () => void) {
     const binding = createBinding(object, property as any)
 
     return (
         <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
             <label label={caption} xalign={0} cssName="settings-param-caption" />
-            <label label={binding.as(val => String(val))} cssName="settings-param-value" halign={Gtk.Align.START} />
+            <box cssName={"settings-param-value-box"}><label label={binding.as(val => String(val))} cssName="settings-param-value" hexpand={false} halign={Gtk.Align.START} /></box>
+            <button visible={buttonVisible} onClicked={onButtonClick} cssName="settings-button">{buttonCaption}</button>
         </box>
     );
 }
@@ -27,17 +28,17 @@ export function BluetoothSettings() {
             <label label="Properties" halign={Gtk.Align.START} cssName="section-heading" />
 
             <box spacing={10} orientation={Gtk.Orientation.VERTICAL} cssName="section-background">
-                {BindBluetoothAdapterProperty("Name", adapter, "name")}
-                {BindBluetoothAdapterProperty("Address", adapter, "address")}
-                {BindBluetoothAdapterProperty("Powered", adapter, "powered")}
-                {BindBluetoothAdapterProperty("Pairable", adapter, "pairable")}
-                {BindBluetoothAdapterProperty("Discoverable", adapter, "discoverable")}
+                {BindBluetoothAdapterProperty("Name", adapter, "name", false, "")}
+                {BindBluetoothAdapterProperty("Address", adapter, "address", false, "")}
+                {BindBluetoothAdapterProperty("Powered", adapter, "powered", true, "Toggle", () => { adapter?.set_powered(!adapter?.get_powered()); })}
+                {BindBluetoothAdapterProperty("Pairable", adapter, "pairable", true, "Pair", () => { adapter?.set_pairable(!adapter?.get_pairable()); })}
+                {BindBluetoothAdapterProperty("Discoverable", adapter, "discoverable", true, "Discover", () => { adapter?.set_discoverable(!adapter?.get_discoverable()); })}
             </box>
 
             <label label="Devices" halign={Gtk.Align.START} cssName="section-heading" />
 
             <box spacing={10} orientation={Gtk.Orientation.VERTICAL} cssName="section-background">
-                
+
             </box>
 
         </box>
