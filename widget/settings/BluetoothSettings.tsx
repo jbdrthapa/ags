5a42b470f67a342ebc03c02edf66613b6c32fd69
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-05-11 20:02:23 -0700

    Adding support to toggle discovery of bluetooth devices.

--- widget/settings/BluetoothSettings.tsx
+++ widget/settings/BluetoothSettings.tsx
@@ -33,6 +33,26 @@ export function BluetoothSettings() {
                 {BindBluetoothAdapterProperty("Discoverable", adapter, "discoverable", true, "Discover", () => { adapter?.set_discoverable(!adapter?.get_discoverable()); })}
             </box>
 
+            <box spacing={10}>
+                <label label="Discover" />
+
+                <button onClicked={() => {
+                    if (!adapter?.discovering) {
+                        console.log("Starting discovery");
+                        adapter?.start_discovery();
+                    }
+                    else {
+                        console.log("Stopping discovery");
+                        adapter?.stop_discovery();
+                    }
+                }
+                }
+                    cssName="settings-button">Toggle
+                </button>
+
+                <button onClicked={() => { console.log("Stopping discovery"); adapter?.stop_discovery() }} cssName="settings-button">Stop</button>
+            </box>
+
             <box orientation={Gtk.Orientation.HORIZONTAL} spacing={20}>
                 <box orientation={Gtk.Orientation.VERTICAL}>
                     <label label="Discovery" halign={Gtk.Align.START} cssName="section-heading" />
@@ -50,6 +70,11 @@ export function BluetoothSettings() {
                                         return (
                                             <box spacing={10} orientation={Gtk.Orientation.VERTICAL}>
                                                 <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
+                                                    <label
+                                                        label={createBinding(device, "name").as(name => name || device.address || "Unknown Device")}
+                                                        cssName="settings-param-value-compact"
+                                                        halign={Gtk.Align.START}
+                                                    />
                                                     <label
                                                         label={createBinding(device, "name").as(name => name || device.address || "Unknown Device")}
                                                         cssName="settings-param-value-compact"
