0e844dc3d1934fbe8495548b2f2f8ef0b52b3ce3
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-05-16 13:59:52 -0700

    Adding Osd file to hold all Osd's, fixing by not showing initial Osd subscription.

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import style from "./styles/main.scss"
 import Bar from "./widget/Bar"
 import Settings from "./widget/settings/Settings"
 import NotificationPopups from "./widget/modules-right/NotificationPopups"
-import OsdVolume from "./widget/osd/OsdVolume"
+import Osd from "./widget/osd/Osd"
 
 app.set_application_id("org.js-shell")
 app.version = "1.0"
@@ -17,7 +17,7 @@ app.start({
       Bar(monitor)
       Settings(monitor)
       NotificationPopups(monitor)
-      OsdVolume(monitor)
+      Osd(monitor)
     }
   },
 })
--- widget/osd/OsdVolume.tsx
+++ widget/osd/OsdVolume.tsx
@@ -4,6 +4,7 @@ import Wp from "gi://AstalWp"
 
 const ANIMATION_TIME = 2000
 let delayId: any = null
+let initCount = 2
 
 export default function OsdVolume(gdkmonitor: Gdk.Monitor) {
     const speaker = Wp.get_default()?.get_audio().defaultSpeaker
@@ -16,6 +17,17 @@ export default function OsdVolume(gdkmonitor: Gdk.Monitor) {
     const [visible, setVisible] = createState(false)
 
     function showOsd() {
+
+        // Added so it doesn't show the OSD when initializing. 
+        // This is due to 2 subscription to WirePlumber signals ("notify::volume","notify::mute").
+
+        if (initCount > 0) {
+            initCount--;
+            console.log("osd window is not created, returning");
+
+            return;
+        }
+
         // FIXED: Call the setter function directly
         setVisible(true)
 
@@ -32,33 +44,34 @@ export default function OsdVolume(gdkmonitor: Gdk.Monitor) {
     speaker.connect("notify::volume", () => showOsd())
     speaker.connect("notify::mute", () => showOsd())
 
-    return (
-        <window
-            name="volume-osd"
-            gdkmonitor={gdkmonitor}
-            cssClasses={["osd-window"]}
-            namespace="volume-osd"
-            anchor={Astal.WindowAnchor.BOTTOM}
-            layer={Astal.Layer.OVERLAY}
-            visible={visible} // Pass the read-only accessor object here
-        >
-            <box cssClasses={["osd-box"]} orientation={Gtk.Orientation.VERTICAL} spacing={8}>
-                <image
-                    iconName={iconBinding}
-                    pixelSize={48}
-                />
-                <label
-                    label={volumeBinding.as(v => `${Math.round(v * 100)}%`)}
-                />
-                <levelbar
-                    widthRequest={100}
-                    heightRequest={25}
-                    cssClasses={["osd-bar"]}
-                    value={volumeBinding.as(v => v)}
-                    valign={Gtk.Align.CENTER}
-                    hexpand={true}
-                />
-            </box>
-        </window>
-    )
+    const osdWindow = (<window
+        name="volume-osd"
+        gdkmonitor={gdkmonitor}
+        cssClasses={["osd-window"]}
+        namespace="volume-osd"
+        anchor={Astal.WindowAnchor.BOTTOM}
+        layer={Astal.Layer.OVERLAY}
+        visible={visible} // Pass the read-only accessor object here
+    >
+        <box cssClasses={["osd-box"]} orientation={Gtk.Orientation.VERTICAL} spacing={8}>
+            <image
+                iconName={iconBinding}
+                pixelSize={48}
+            />
+            <label
+                label={volumeBinding.as(v => `${Math.round(v * 100)}%`)}
+            />
+            <levelbar
+                widthRequest={100}
+                heightRequest={25}
+                cssClasses={["osd-bar"]}
+                value={volumeBinding.as(v => v)}
+                valign={Gtk.Align.CENTER}
+                hexpand={true}
+            />
+        </box>
+    </window>
+    ) as Gtk.Window;
+
+    return osdWindow;
 }
