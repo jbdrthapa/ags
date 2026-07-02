2fba3ea9233fe5e863273cf1fe4aaf36316efafa
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-07-02 13:58:08 -0700

    Adding speaker channel images, changing the layout to be horizontal.

--- widget/settings/AudioSettings.tsx
+++ widget/settings/AudioSettings.tsx
@@ -34,26 +34,32 @@ export function AudioSettings() {
 
     const CONFIG_DIR = `${Utils.GetUserConfigDirectory()}/ags`;
 
-    const CHANNEL_LAYOUT: Record<number, string> = {
-        1: `${CONFIG_DIR}/assets/speaker_layout/mono.svg`,
-        2: `${CONFIG_DIR}/assets/speaker_layout/stereo.svg`,
-        4: `${CONFIG_DIR}/assets/speaker_layout/quad.svg`,
-        6: `${CONFIG_DIR}/assets/speaker_layout/5p1_surround.svg`,
-        8: `${CONFIG_DIR}/assets/speaker_layout/7p1_surround.svg`
+    const CHANNEL_LAYOUT: Record<number, [string, string]> = {
+        1: [`${CONFIG_DIR}/assets/speaker_layout/mono.svg`, "Mono"],
+        2: [`${CONFIG_DIR}/assets/speaker_layout/stereo.svg`, "Stereo"],
+        4: [`${CONFIG_DIR}/assets/speaker_layout/quad.svg`, "Quadraphonic"],
+        6: [`${CONFIG_DIR}/assets/speaker_layout/5p1_surround.svg`, "5.1 Surround"],
+        8: [`${CONFIG_DIR}/assets/speaker_layout/7p1_surround.svg`, "7.1 Surround"]
     };
 
-    const NODE_STATE: Record<number, string> = {
-        0: `${CONFIG_DIR}/assets/node_state/node_state_0_creating.svg`,              // ASTAL_WP_NODE_STATE_CREATING
-        1: `${CONFIG_DIR}/assets/node_state/node_state_1_suspended.svg`,             // ASTAL_WP_NODE_STATE_SUSPENDED
-        2: `${CONFIG_DIR}/assets/node_state/node_state_2_idle.svg`,                  // ASTAL_WP_NODE_STATE_IDLE
-        3: `${CONFIG_DIR}/assets/node_state/node_state_3_running.svg`,               // ASTAL_WP_NODE_STATE_RUNNING
+    const NODE_STATE: Record<number, [string, string]> = {
+        0: [`${CONFIG_DIR}/assets/node_state/node_state_0_creating.svg`, "Creating"],              // ASTAL_WP_NODE_STATE_CREATING
+        1: [`${CONFIG_DIR}/assets/node_state/node_state_1_suspended.svg`, "Suspended"],             // ASTAL_WP_NODE_STATE_SUSPENDED
+        2: [`${CONFIG_DIR}/assets/node_state/node_state_2_idle.svg`, "Idle"],                  // ASTAL_WP_NODE_STATE_IDLE
+        3: [`${CONFIG_DIR}/assets/node_state/node_state_3_running.svg`, "Running"],               // ASTAL_WP_NODE_STATE_RUNNING
     };
 
-    const SPEAKER_CHANNEL: Record<string, string> = {
-        "FL": "Front Left",
-        "FR": "Front Right",
-        "SL": "Surround Left",
-        "SR": "Surround Right",
+    const GENERAL: Record<string, [string, string]> = {
+        "lock": [`${CONFIG_DIR}/assets/general/locked.svg`, "Lock"],
+    };
+
+    const SPEAKER_CHANNEL: Record<string, [string, string]> = {
+        "FL": [`${CONFIG_DIR}/assets/channel/front-left.svg`, "Front Left"],
+        "FR": [`${CONFIG_DIR}/assets/channel/front-right.svg`, "Front Right"],
+        "FC": [`${CONFIG_DIR}/assets/channel/front-center.svg`, "Front Center"],
+        "LFE": [`${CONFIG_DIR}/assets/channel/subwoofer.svg`, "Subwoofer"],
+        "SL": [`${CONFIG_DIR}/assets/channel/surround-left.svg`, "Surround Left"],
+        "SR": [`${CONFIG_DIR}/assets/channel/surround-right.svg`, "Surround Right"],
     };
 
     const MEDIA_CLASS: Record<number, string> = {
@@ -74,145 +80,166 @@ export function AudioSettings() {
 
             <label label="Audio Settings" halign={Gtk.Align.START} cssName="section-heading" />
 
-            <For each={speakersBinding}>
+            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={40}>
+                <For each={speakersBinding}>
 
-                {(speaker) => {
+                    {(speaker) => {
 
-                    const description = createComputed(() => {
-                        const rawDesc = createBinding(speaker, "description");
-                        return rawDesc() ?? "Unknown Device";
-                    });
+                        const description = createComputed(() => {
+                            const rawDesc = createBinding(speaker, "description");
+                            return rawDesc() ?? "Unknown Device";
+                        });
 
-                    const volumeIcon = createComputed(() => {
-                        const rawIcon = createBinding(speaker, "volumeIcon");
-                        return rawIcon() ?? "audio-volume-muted";
-                    });
+                        const volumeIcon = createComputed(() => {
+                            const rawIcon = createBinding(speaker, "volumeIcon");
+                            return rawIcon() ?? "audio-volume-muted";
+                        });
 
-                    const nodeStateIconPath = createComputed(() => {
-                        const rawState = createBinding(speaker, "state");
-                        return NODE_STATE[rawState()] ?? "?";
-                    });
+                        const nodeStateIconPath = createComputed(() => {
+                            const rawState = createBinding(speaker, "state");
+                            return NODE_STATE[rawState()][0] ?? "?";
+                        });
+                        const nodeState = createComputed(() => {
+                            const rawState = createBinding(speaker, "state");
+                            return NODE_STATE[rawState()][1] ?? "?";
+                        });
 
-                    const mediaClassIconPath = createComputed(() => {
-                        const rawMediaClass = createBinding(speaker, "mediaClass");
-                        return MEDIA_CLASS[rawMediaClass()] ?? "?";
-                    });
+                        const mediaClassIconPath = createComputed(() => {
+                            const rawMediaClass = createBinding(speaker, "mediaClass");
+                            return MEDIA_CLASS[rawMediaClass()] ?? "?";
+                        });
 
-                    const layoutIconPath = createComputed(() => {
-                        const rawLayout = createBinding(speaker, "channels");
-                        const channelsArray = rawLayout() || [];
-                        const count = channelsArray.length;
-                        return CHANNEL_LAYOUT[count] ?? "?";
-                    });
+                        const layoutIconPath = createComputed(() => {
+                            const rawLayout = createBinding(speaker, "channels");
+                            const channelsArray = rawLayout() || [];
+                            const count = channelsArray.length;
+                            return CHANNEL_LAYOUT[count][0] ?? "?";
+                        });
 
-                    const isDefault = createBinding(speaker, "isDefault");
+                        const layoutName = createComputed(() => {
+                            const rawLayout = createBinding(speaker, "channels");
+                            const channelsArray = rawLayout() || [];
+                            const count = channelsArray.length;
+                            return CHANNEL_LAYOUT[count][1] ?? "?";
+                        });
 
-                    const isMute = createBinding(speaker, "mute");
+                        const isDefault = createBinding(speaker, "isDefault");
 
-                    const lockChannels = createBinding(speaker, "lockChannels");
+                        const isMute = createBinding(speaker, "mute");
 
-                    return (
-                        <box spacing={10} orientation={Gtk.Orientation.VERTICAL} cssName="section-background">
+                        const lockIconPath = GENERAL["lock"][0];
 
-                            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                                <label label={description} cssName="settings-param-value" halign={Gtk.Align.START} />
-                            </box>
-                            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={3}>
-                                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={3}>
-                                    <label label="Layout" xalign={0} cssName="settings-param-caption" />
-                                    <image file={layoutIconPath} iconSize={Gtk.IconSize.LARGE} cssName="settings-param-icon" halign={Gtk.Align.START} />
-                                </box>
+                        const lockLabel = GENERAL["lock"][1];
 
-                                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={3}>
-                                    <label label="Class" xalign={0} cssName="settings-param-caption" />
+                        const lockChannels = createBinding(speaker, "lockChannels");
+
+                        return (
+                            <box spacing={5} orientation={Gtk.Orientation.VERTICAL} cssName="section-background">
+
+                                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={20}>
                                     <image file={mediaClassIconPath} iconSize={Gtk.IconSize.LARGE} cssName="settings-param-icon" halign={Gtk.Align.START} />
+                                    <label label={description} cssName="settings-param-heading" halign={Gtk.Align.START} />
                                 </box>
 
                                 <box orientation={Gtk.Orientation.HORIZONTAL} spacing={3}>
-                                    <label label="State" xalign={0} cssName="settings-param-caption" />
-                                    <image file={nodeStateIconPath} iconSize={Gtk.IconSize.LARGE} cssName="settings-param-icon" halign={Gtk.Align.START} />
+                                    <box orientation={Gtk.Orientation.VERTICAL} spacing={3}>
+                                        <image file={layoutIconPath} tooltipText={layoutName} pixelSize={48} cssName="settings-param-icon" halign={Gtk.Align.CENTER} />
+                                        <label label={layoutName} xalign={0.5} cssName="settings-param-caption" />
+                                    </box>
+
+                                    <box orientation={Gtk.Orientation.VERTICAL} spacing={3}>
+                                        <image file={nodeStateIconPath} tooltipText={nodeState} pixelSize={48} cssName="settings-param-icon" halign={Gtk.Align.CENTER} />
+                                        <label label={nodeState} xalign={0.5} cssName="settings-param-caption" />
+                                    </box>
+
+                                    <box orientation={Gtk.Orientation.HORIZONTAL} spacing={3}>
+                                        <label label="Muted" xalign={0} cssName="settings-param-caption" />
+                                        <Gtk.Switch
+                                            active={isMute}
+                                            hexpand={false}
+                                            vexpand={false}
+                                            halign={Gtk.Align.END}
+                                            valign={Gtk.Align.CENTER}
+                                            onNotifyActive={(self) => {
+                                                speaker.mute = self.active;
+                                            }} />
+                                    </box>
                                 </box>
 
-                                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={3}>
-                                    <label label="Muted" xalign={0} cssName="settings-param-caption" />
+                                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
+                                    <label label="Icon" xalign={0} cssName="settings-param-caption" />
+                                    <label label={volumeIcon} cssName="settings-param-value" halign={Gtk.Align.START} />
+                                </box>
+
+                                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
+                                    <label label="Default" xalign={0} cssName="settings-param-caption" />
                                     <Gtk.Switch
-                                        active={isMute}
+                                        active={isDefault}
                                         hexpand={false}
                                         vexpand={false}
                                         halign={Gtk.Align.END}
                                         valign={Gtk.Align.CENTER}
                                         onNotifyActive={(self) => {
-                                            speaker.mute = self.active;
+                                            speaker.isDefault = self.active;
                                         }} />
                                 </box>
+
+                                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={40}>
+                                    <box orientation={Gtk.Orientation.VERTICAL} spacing={10}>
+                                        <For each={createComputed(() => speaker.channels || [])}>
+                                            {(channel: any) => {
+
+                                                const channelVolume = createBinding(channel, "volume");
+
+                                                const volumeText = createComputed(() => `${Math.round(channelVolume() * 100)}`);
+
+                                                const channelIconPath = createComputed(() => {
+                                                    const rawChannelName = createBinding(channel, "name");
+                                                    return SPEAKER_CHANNEL[rawChannelName()][0];
+                                                })
+
+                                                const channelName = createComputed(() => {
+                                                    const rawChannelName = createBinding(channel, "name");
+                                                    return SPEAKER_CHANNEL[rawChannelName()][1];
+                                                })
+
+                                                return (
+
+                                                    <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10} marginStart={15}>
+                                                        <image file={channelIconPath} tooltipText={channelName} pixelSize={48} cssName="settings-param-icon" halign={Gtk.Align.CENTER} />
+                                                        <slider
+                                                            cssClasses={["slider-control"]}
+                                                            tooltipText={volumeText}
+                                                            widthRequest={280}
+                                                            onChangeValue={({ value }) => channel.set_volume(value)}
+                                                            value={channelVolume} />
+                                                        <label label={volumeText} cssName={"settings-param-value-compact"} css="min-width: 50px;" />
+                                                    </box>
+
+                                                );
+                                            }}
+
+                                        </For>
+                                    </box>
+                                    <box orientation={Gtk.Orientation.HORIZONTAL} spacing={3}>
+                                        <image file={lockIconPath} pixelSize={48} tooltipText={lockLabel} cssName="settings-param-icon" halign={Gtk.Align.START} />
+                                        <Gtk.Switch
+                                            active={lockChannels}
+                                            hexpand={false}
+                                            vexpand={false}
+                                            halign={Gtk.Align.END}
+                                            valign={Gtk.Align.CENTER}
+                                            onNotifyActive={(self) => {
+                                                speaker.lockChannels = self.active;
+                                            }} />
+                                    </box>
+                                </box>
                             </box>
+                        )
+                    }}
+                </For >
+            </box>
 
-                            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                                <label label="Icon" xalign={0} cssName="settings-param-caption" />
-                                <label label={volumeIcon} cssName="settings-param-value" halign={Gtk.Align.START} />
-                            </box>
-
-                            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                                <label label="Default" xalign={0} cssName="settings-param-caption" />
-                                <Gtk.Switch
-                                    active={isDefault}
-                                    hexpand={false}
-                                    vexpand={false}
-                                    halign={Gtk.Align.END}
-                                    valign={Gtk.Align.CENTER}
-                                    onNotifyActive={(self) => {
-                                        speaker.isDefault = self.active;
-                                    }} />
-                            </box>
-
-                            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
-                                <label label="Lock" xalign={0} cssName="settings-param-caption" />
-                                <Gtk.Switch
-                                    active={lockChannels}
-                                    hexpand={false}
-                                    vexpand={false}
-                                    halign={Gtk.Align.END}
-                                    valign={Gtk.Align.CENTER}
-                                    onNotifyActive={(self) => {
-                                        speaker.lockChannels = self.active;
-                                    }} />
-                            </box>
-
-
-
-                            <For each={createComputed(() => speaker.channels || [])}>
-                                {(channel: any) => {
-
-                                    const channelVolume = createBinding(channel, "volume");
-
-                                    const volumeText = createComputed(() => `${Math.round(channelVolume() * 100)}%`);
-
-                                    const channelName = createComputed(() => {
-                                        const rawChannelName = createBinding(channel, "name");
-                                        return SPEAKER_CHANNEL[rawChannelName()];
-                                    })
-
-                                    return (
-                                        <box orientation={Gtk.Orientation.HORIZONTAL} spacing={10} marginStart={15}>
-                                            <label label={channelName} />
-                                            <label label={volumeText} />
-                                            <slider
-                                                cssClasses={["slider-control"]}
-                                                tooltipText={volumeText}
-                                                widthRequest={280}
-                                                onChangeValue={({ value }) => channel.set_volume(value)}
-                                                value={channelVolume} />
-
-                                        </box>
-                                    );
-                                }}
-
-                            </For>
-
-                        </box>
-                    )
-                }}
-            </For >
         </box >
     );
 }
