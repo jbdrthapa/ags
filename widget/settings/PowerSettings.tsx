eea91b7aa968eb4f1256a0e1e48ee80e5b5ba64a
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-07-03 22:21:39 -0700

    Adding power related assets and show battery percentage label.

--- widget/settings/PowerSettings.tsx
+++ widget/settings/PowerSettings.tsx
@@ -5,15 +5,37 @@ import Utils from "../../utils";
 
 export function PowerSettings() {
 
-    const ab = AstalBattery.get_default();
+    const upower = new AstalBattery.UPower();
 
-    
+    const devicesBinding = createComputed(() => upower.devices || []);
 
     return (
 
         <box orientation={Gtk.Orientation.VERTICAL} spacing={10}>
 
             <label label="Power Settings" halign={Gtk.Align.START} cssName="section-heading" />
+
+            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={40}>
+                <For each={devicesBinding}>
+                    {(device) => {
+
+                        const vendor = createBinding(device, "vendor");
+
+                        const percentage = createComputed(() => {
+                            const rawPercentage = createBinding(device, "percentage");
+                            const formattedPercentage = `${Math.round((rawPercentage() ?? 0) * 100)}%`;
+                            return formattedPercentage ?? "Unknown Device";
+                        });
+
+                        return (
+                            <box orientation={Gtk.Orientation.VERTICAL} spacing={10} cssName="section-background">
+                                <label label={vendor} cssName="settings-param-heading" />
+                                <label label={percentage} />
+                            </box>
+                        );
+                    }}
+                </For>
+            </box>
 
 
         </box >
