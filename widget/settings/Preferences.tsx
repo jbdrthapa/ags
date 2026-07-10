f022233bf0f9e1a45ccea2167fb0eb75d5f17b13
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-07-09 20:29:44 -0700

    Adding support for loading the the correct units from stored value.

--- widget/settings/Preferences.tsx
+++ widget/settings/Preferences.tsx
@@ -4,18 +4,45 @@ import Config from "../../conf/Config";
 
 export function Preferences() {
 
+    // Units 
     const UNIT_OPTIONS = [
         { display: "Imperial", value: "imperial" },
         { display: "Metric", value: "metric" }
     ];
-    const unitModel = Gtk.StringList.new(UNIT_OPTIONS.map(opt => opt.display));
 
-    function onUnitSelected(widget: Gtk.DropDown) {
-        const activeIndex = widget.get_selected();
+    const unitModel = Gtk.StringList.new(UNIT_OPTIONS.map(opt => opt.display));
+    
+    const unitsDropDown = new Gtk.DropDown({
+        model: unitModel,
+        selected: 0,
+        hexpand: false,
+        width_request: 200,
+        halign: Gtk.Align.START
+    });
+    unitsDropDown.connect('notify::selected', (dropDown) => {
+        const activeIndex = dropDown.get_selected();
         const selectedValue = UNIT_OPTIONS[activeIndex].value;
 
         Config.SetMeasurementUnits(selectedValue);
+    });
+
+    function loadUnits() {
+        const units = Config.GetMeasurementUnits();
+        const targetIndex = UNIT_OPTIONS.findIndex(option => option.value === units);
+
+        if (targetIndex == -1) {
+            console.log("Unable to load correct value for units.")
+            return;
+        }
+
+        unitsDropDown.selected = targetIndex;
     }
+
+    function loadPreferences() {
+        loadUnits();
+    }
+
+    loadPreferences();
 
     return (
         <box orientation={Gtk.Orientation.VERTICAL} spacing={10}>
@@ -26,14 +53,7 @@ export function Preferences() {
 
                 <box spacing={10} orientation={Gtk.Orientation.VERTICAL} cssName="section-background">
                     <label label={"Units"} halign={Gtk.Align.START} hexpand={false} />
-                    <Gtk.DropDown
-                        model={unitModel}
-                        selected={0}
-                        onNotifySelected={onUnitSelected}
-                        hexpand={false}
-                        widthRequest={200}
-                        halign={Gtk.Align.START}
-                    />
+                    {unitsDropDown}
                 </box>
                 <box spacing={10} orientation={Gtk.Orientation.VERTICAL} cssName="section-background">
 
