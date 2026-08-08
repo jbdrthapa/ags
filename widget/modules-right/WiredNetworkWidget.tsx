e935277152cbdad9d4050319b2136309f979fe29
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-08-08 06:31:30 -0700

    Adding some details to the Wired network.

--- widget/modules-right/WiredNetworkWidget.tsx
+++ widget/modules-right/WiredNetworkWidget.tsx
@@ -1,13 +1,44 @@
 import Gtk from "gi://Gtk?version=4.0";
 import { AccordionController } from "./AccordionController";
 import { PillWidget } from "./PillWidget";
-
-const accordion = new AccordionController();
+import { createBinding, createComputed } from "gnim";
+import Network from "gi://AstalNetwork"
 
 export function WiredNetworkWidget(controller: AccordionController) {
+
+    let detail = "";
+
+    const network = Network.get_default();
+
+    const wired = network.wired;
+
+    if (!wired) {
+        detail = "Unavailable"
+        return;
+    }
+
+    const device = createBinding(wired, "device");
+
+    const iconName = createBinding(wired, "iconName");
+
+    const deviceInterface = createComputed(() => {
+        return device().interface;
+    });
+
+    const macAddress = createComputed(() => {
+        return device().hwAddress;
+    });
+
     const content = (
         <box orientation={Gtk.Orientation.VERTICAL} cssName="pill-content">
-            <label label="Wired Network" />
+            <box orientation={Gtk.Orientation.HORIZONTAL}>
+                <image iconSize={Gtk.IconSize.NORMAL} iconName={iconName} />
+                <label label={deviceInterface} halign={Gtk.Align.START} />
+            </box>
+            <box orientation={Gtk.Orientation.HORIZONTAL}>
+                <label label="MAC" halign={Gtk.Align.START} />
+                <label label={macAddress} halign={Gtk.Align.START} />
+            </box>
         </box>
     ) as Gtk.Box;
 
@@ -16,7 +47,7 @@ export function WiredNetworkWidget(controller: AccordionController) {
         controller: controller,
         iconName: "󰈀",
         title: "Wired",
-        detail: "Connected",
+        detail: deviceInterface,
         content,
     });
 }
